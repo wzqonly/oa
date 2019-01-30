7a3c08676a4389543af9f682d69e1affdd821039
wzqonly <w19965292home@163.com>
2019-01-30 22:18:16 +0800

    搭建ssh框架环境

---
+++ src/main/java/com/oa/entity/Employee.java
@@ -0,0 +1,79 @@
+package com.oa.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by zonly on 2019/1/30.
+ */
+@Entity
+@Table(name = "t_employee", schema = "oa", catalog = "")
+public class Employee {
+    private String id;
+    private String userName;
+    private String password;
+    private String email;
+
+    @Id
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "user_name")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != null ? !id.equals(employee.id) : employee.id != null) return false;
+        if (userName != null ? !userName.equals(employee.userName) : employee.userName != null) return false;
+        if (password != null ? !password.equals(employee.password) : employee.password != null) return false;
+        if (email != null ? !email.equals(employee.email) : employee.email != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zonly
+  Date: 2019/1/30
+  Time: 14:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--<jsp:forward page="index.jsp" />--%>
+hhh
